b6b76b90fc29e69cc69edfe8472ecd1b83cf1836
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-09-12 02:08:15 -0400

    Added title card that can move

---
+++ public/entities/TitleCard.js
@@ -0,0 +1,74 @@
+class TitleCard extends Entity {
+    constructor(canvasObj, x, y, imgW, imgH, imgName) {
+        super(canvasObj, x, y);
+        this.imgW = imgW;
+        this.imgH = imgH;
+        this.imgName = imgName;
+        this.coordinatesSet = false;
+        this.fromX = 0;
+        this.fromY = 0;
+        this.toX = 0;
+        this.toY = 0;
+        this.currentX = 0;
+        this.currentY = 0;
+        this.dx = 1;
+        this.dy = 1;
+        this.eraseOnDestination = true;
+    }
+
+    /**
+     * Set the coordinates so the image can move from point A to point B
+     * 
+     * @param {int} fromX 
+     * @param {int} fromY 
+     * @param {int} toX 
+     * @param {int} toY 
+     * @param {boolean} eraseOnDestination 
+     */
+    setCoordinates(fromX, fromY, toX, toY, eraseOnDestination) {
+        this.fromX = fromX;
+        this.fromY = fromY;
+        this.toX = toX;
+        this.toY = toY;
+        this.currentX = fromX;
+        this.currentY = fromY;
+        this.coordinatesSet = true;
+        this.dx = (fromX < toX) ? 1 : -1;
+        this.dy = (fromY < toY) ? 1 : -1;
+        this.eraseOnDestination = eraseOnDestination;
+    }
+
+    /**
+     * Draws the image to screen
+     */
+    draw() {
+        if (this.coordinatesSet) {
+            let img = new Image();
+            img.src = `assets/images/${this.imgName}`;
+            img.onload = this.onImageLoaded(img);
+        }
+    }
+
+    /**
+     * Handles moving and drawing image to screen
+     * 
+     * @param {Image} img 
+     */
+    onImageLoaded(img) {
+        if ((this.currentX >= this.toX && this.currentY >= this.toY) || 
+        (this.currentX <= this.toX && this.currentY <= this.toY)) {
+            this.ctx.drawImage(img, this.currentX, this.currentY);
+
+            if (this.currentX != this.toX) {
+                this.currentX += this.dx;
+            }
+            if (this.currentY != this.toY) {
+                this.currentY += this.dy;
+            }
+        } else {
+            if (!this.eraseOnDestination) {
+                this.ctx.drawImage(img, this.currentX, this.currentY);
+            }
+        }
+    }
+}
--- public/game.js
+++ public/game.js
@@ -3,17 +3,33 @@
  * 
  * Contains all game functionality and logic
  */
-let assetsDir = "./assets/";
-let audioDir = `${assetsDir}audio/`;
-let imagesDir = `${assetsDir}images/`;
+const assetsDir = "./assets/";
+const audioDir = `${assetsDir}audio/`;
+const imagesDir = `${assetsDir}images/`;
 
+const spaceBarKeyCode = 32;
 const bgl1 = document.getElementById("bgl1");
 const bgl2 = document.getElementById("bgl2");
 const fgl1 = document.getElementById("fgl1");
 const fgl2 = document.getElementById("fgl2");
 const bgl1Ctx = bgl1.getContext("2d");
+const fgl2Ctx = fgl2.getContext("2d");
 
 let bootCompleteFlag = false;
+
+// This is the game object
+let game;
+
+// Press space
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode == spaceBarKeyCode) {
+        if (bootCompleteFlag) {
+            if (game.isMenuMode) {
+                alert("Hello World!");
+            }
+        }
+    }
+});
 
 class Game {
 
@@ -23,7 +39,7 @@ class Game {
 
         // game variables
         this.bootTime = 7500;
-        this.isMenuMode = false;
+        this.isMenuMode = true;
         this.bgl1Speed = 50;
         this.bgl2Speed = 20;
         this.fgl1Speed = 3;
@@ -88,6 +104,22 @@ class Game {
     }
 
     /**
+     * Play a music track
+     * 
+     * @param {strong} track 
+     * @param {boolean} isLoop 
+     * @param {float} volume 
+     * @returns {Audio}
+     */
+    playTrack(track, isLoop, volume) {
+        let gameMusic = new Audio(track);
+        gameMusic.loop = isLoop;
+        gameMusic.volume = volume;
+        gameMusic.play();
+        return gameMusic;
+    }
+
+    /**
      * Goes to the main menu
      */
     menu() {
@@ -106,6 +138,24 @@ class Game {
      */
     update() {
         
+    }
+
+    //FIXME: Make this a class
+    dropTitle() {
+        function drop() {
+            fgl2Ctx.globalAlpha += 0.02;
+            fgl2Ctx.clearRect(0, 0, bgl1.width, bgl1.height);
+            fgl2Ctx.drawImage(bootLogo, 0, 0);
+
+            if (bgl1Ctx.globalAlpha < 1.0) {
+                requestAnimationFrame(fadeIn);    
+            }
+        }
+        requestAnimationFrame(drop);
+    }
+
+    liftTitle() {
+
     }
 
     /**
@@ -157,36 +207,37 @@ class Game {
  * Loops through game logic to play the game
  */
 async function gameLoop() {
-    let game = new Game(bgl1, bgl1Ctx);
+    game = new Game(bgl1, bgl1Ctx);
     bgl2.style.display = "none";
     fgl1.style.display = "none";
 
     // Boot game
     await game.sleep(800).then(() => {
-        // just a little flair to make it look like it needs time to load
         requestAnimationFrame(game.boot);
     });
     await game.awaitBootFinish().then(() => {
         bootCompleteFlag = true;
+        bgl1Ctx.globalAlpha = 1;
     });
-    bgl1Ctx.globalAlpha = 1;
 
-    // Init layers
+    // Init layers, audio, and starting sprites
     let canvasBgl1 = game.initBgl1();
     let canvasBgl2 = game.initBgl2();
     let canvasFgl1 = game.initFgl1();
+    let title = new TitleCard({canvas: fgl2}, 0, 0, 256, 128, "title.png");
     game.showLayers();
-
-    // Play the game music
-    let gameMusic = new Audio(`${audioDir}steviaSphere_Dolphin.mp3`);
-    gameMusic.loop = true;
-    gameMusic.play();
+    game.playTrack(`${audioDir}steviaSphere_Dolphin.mp3`, true, 0.5)
+    title.setCoordinates(330, -138, 330, 60, false);
 
     // This is the game loop
     function loop() {
+
         canvasBgl1.draw();
         canvasBgl2.draw();
         canvasFgl1.draw();
+
+        fgl2.getContext("2d").clearRect(0, 0, 920, 540);
+        title.draw();
 
         requestAnimationFrame(loop);
     }
